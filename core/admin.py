6f63f9c7f6457e991ebaa88509d9ef6db4a324b3
KauaneDelvoss <delvoss.ribas@gmail.com>
2022-07-05 15:16:37 -0300

    book table

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.db import models
 from django.forms import URLField
 
-from core.models import Categoria, Editora
+from core.models import Categoria, Editora, Autor, Livros
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livros)
--- core/models.py
+++ core/models.py
@@ -16,3 +16,22 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=225)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+class Livros(models.Model):
+    titulo = models.CharField(max_length=225)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.titulo, self.quantidade
