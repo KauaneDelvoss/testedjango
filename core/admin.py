054c2b51468122d413f60567a8fc209a61435c2f
gustavotrentinidev <gustavotrentini22@gmail.com>
2022-07-05 14:13:59 -0300

    cade o black

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from django.db import models
 from django.forms import URLField
 
--- core/models.py
+++ core/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
+
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
